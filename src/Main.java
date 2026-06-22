82909f859c303cb8c57f51b03d8348873a4f23ea
Buddhila Sankalpa <wildback21@gmail.com>
2026-06-23 00:40:15 +0530

    project start

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 class Main {
     public static void main(String[] args) {
-        try {
+
             LoginFrame Loginframe = new LoginFrame();
             Loginframe.setVisible(true);
-        }
 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 }

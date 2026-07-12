ccb7a517a6e0d89f9e596d46d7d4ef264810bef0
Sehan <dkshashika652@gmail.com>
2026-07-12 11:35:38 +0530

    add button images

--- src/RoundedButton.java
+++ src/RoundedButton.java
@@ -5,43 +5,39 @@ public class RoundedButton extends JButton {
 
     private int radius;
 
-    public RoundedButton(String text) {
-        this(text, 18);
-    }
-
     public RoundedButton(String text, int radius) {
         super(text);
         this.radius = radius;
-        setFocusPainted(false);
+
         setContentAreaFilled(false);
+        setFocusPainted(false);
         setBorderPainted(false);
         setOpaque(false);
+
+        setFont(new Font("Arial", Font.BOLD, 18));
+        setBackground(Color.white);
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+
     }
 
-    @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
 
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON
+        );
 
-        g2.setColor(getBackground());
+        if(getModel().isPressed()) {
+            g2.setColor(getBackground().darker());
+        }else if(getModel().isRollover()) {
+            g2.setColor(getBackground().brighter());
+        }else {
+            g2.setColor(getBackground());
+        }
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
 
-        super.paintComponent(g2);
         g2.dispose();
-    }
-
-    @Override
-    protected void paintBorder(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g.create();
-
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-
-        g2.setColor(getBackground().darker());
-        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, radius, radius);
-
-        g2.dispose();
+        super.paintComponent(g);
     }
 }
